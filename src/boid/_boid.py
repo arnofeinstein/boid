488a1f4cf023e54933a2bc2fa28b6294437ec6e6
Arno Feinstein <arno.feinstein@gmail.com>
2026-03-06 16:40:12 +0100

    add class Boid

--- src/boid/_boid.py
+++ src/boid/_boid.py
@@ -1,9 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
 class Boid:
-    def __init__(self, *args):
-        print("Hello from Boid")
-        self.arg = args[0]
+    def move_boid(self, *, dt: float) -> None:
+        """
+        Move the boid based on its velocity
 
+        Args:
+            dt: The time step for the simulation
+        """
+        # Write a function that moves the boid based on its velocity
+        self.position += self.velocity * dt
+        ...
 
-def run_app():
-    my_obj = Boid("Hi!")
-    print(f"{my_obj.arg = }")
+    def plot_trajectory(self, *, dt: float, num_steps: int) -> None:
+        """
+        Plot the trajectory of the boid
+
+        Args:
+            dt: The time step for the simulation
+            num_steps: The number of steps to simulate
+        """
+        # Write the code to plot the trajectory of the boid in an other way
+        traj = np.empty((2, num_steps), dtype=float)
+        for i in range(num_steps):
+            self.move_boid(dt=dt)
+            traj[:, i] = self.position
+
+        _, ax = plt.subplots()
+        ax.plot(*traj, "o-")
+        ax.set_aspect("equal")
+        ...
+
+    def __init__(self, position, velocity):
+        self.position = np.array(position, dtype=float)
+        self.velocity = np.array(velocity, dtype=float)
